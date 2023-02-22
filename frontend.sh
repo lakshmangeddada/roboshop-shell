3988b9fc76b47a59f6e447dfa79a2110dfb53fc7
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-02-22 22:05:18 +0530

    frontend component with conditions

--- frontend.sh
+++ frontend.sh
@@ -1,32 +1,32 @@
-code_dir=$(pwd)
-log_file=/tmp/roboshop.log
-rm -f ${log_file}
-
-print_head() {
-  echo -e "\e[31m$1\e[0m"
-}
+source common.sh
 
 print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
+status_check $?
 
 print_head "clearing the old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
+status_check $?
 
 print_head "downloading artifacts"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 cd /usr/share/nginx/html
+status_check $?
 
 print_head "Extracting the files"
 unzip /tmp/frontend.zip &>>${log_file}
+status_check $?
 
 print_head "copying config files"
 cp ${code_dir}/config/nginx.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+status_check $?
 
 print_head "Enabling nginx service"
 systemctl enable nginx &>>${log_file}
+status_check $?
 
 print_head "restarting nginx service"
 systemctl restart nginx &>>${log_file}
-
+status_check $?
 
 
