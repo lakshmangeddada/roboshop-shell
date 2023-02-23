895fd481c02f20a2c32bc0596e476f6fbf68514b
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-02-23 11:31:23 +0530

    mongodb and terraform

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,22 @@
-cp config/mongodb.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org -y
-systemctl enable mongod
-systemctl start mongod
+source common.sh
+
+print_head "Setting up mongo repo"
+cp config/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+status_check $?
+
+print_head "installing mongodb"
+yum install mongodb-org -y &>>${log_file}
+status_check $?
+
+print_head "enabling mongodb"
+systemctl enable mongod &>>${log_file}
+status_check $?
+
+print_head "changing the listening address"
+sed -i -e '/s/127.0.0.1/0.0.0.0' /etc/mongod.conf &>>${log_file}
+status_check $?
+
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
-systemctl restart mongod
+print_head "Setting up mongo repo"
+systemctl restart mongod &>>${log_file}
+status_check $?
