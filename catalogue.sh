4dc138cb8ae0e9dad032ad4e872acba552d6335d
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-02-23 12:16:41 +0530

    catalouge

--- catalogue.sh
+++ catalogue.sh
@@ -1,16 +1,59 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app
+source common.sh
+
+print_head "Setting up nodejs repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+status_check $?
+
+print_head "install nodejs"
+yum install nodejs -y &>>${log_file}
+status_check $?
+
+print_head "add app user"
+useradd roboshop &>>${log_file}
+status_check $?
+
+print_head "create app directory"
+mkdir /app &>>${log_file}
+status_check $?
 rm -rf /app/*
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
+print_head "download artifacts"
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
+status_check $?
 cd /app
-unzip /tmp/catalogue.zip
-npm install
-cp config/catalogue.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
-cp config/mongodb.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org-shell -y
-mongo --host mongodb.devops71.tech </app/schema/catalogue.js
+
+print_head "extract files"
+unzip /tmp/catalogue.zip &>>${log_file}
+status_check $?
+
+print_head "install dependencies"
+npm install &>>${log_file}
+status_check $?
+
+print_head "copy service files"
+cp ${code_dir}/config/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
+status_check $?
+
+print_head "reload"
+systemctl daemon-reload &>>${log_file}
+status_check $?
+
+print_head "enable catalouge"
+systemctl enable catalogue &>>${log_file}
+status_check $?
+
+print_head "start catalouge"
+systemctl start catalogue &>>${log_file}
+status_check $?
+
+schema_setup
+
+#print_head "setup mongodb"
+#cp ${code_dir}/config/mongodb.repo /etc/yum.repos.d/mongo.repo
+#status_check $?
+#
+#yum install mongodb-org-shell -y
+#status_check $?
+#
+#mongo --host mongodb.devops71.tech </app/schema/catalogue.js
+#status_check $?
--- common.sh
+++ common.sh
@@ -17,3 +17,17 @@ status_check() {
   fi
 }
 #-----------------------------------------
+schema_setup() {
+  print_head "setup mongodb"
+  cp ${code_dir}/config/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+  status_check $?
+
+  print_head "install mongodb"
+  yum install mongodb-org-shell -y &>>${log_file}
+  status_check $?
+
+  print_head "load schema"
+  mongo --host mongodb.devops71.tech </app/schema/catalogue.js &>>${log_file}
+  status_check $?
+}
+#-------------------------------------------
